828c8de5ac62c13ffd19c93a7f0dfc46dbdf8950
DatsOlena <datsolena@gmail.com>
2026-03-16 21:18:11 +0100

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Projects } from '@/sections/Projects'
 import { Testimonials } from '@/sections/Testimonials'
 import { Contact } from '@/sections/Contact'
 import Navbar from '@/layout/Navbar'
+import Footer from '@/layout/Footer'
 
 function App() {
   const [theme, setTheme] = useState(() => {
@@ -38,6 +39,7 @@ function App() {
         <Testimonials />
         <Contact />
       </main>
+      <Footer />
     </div>
   )
 }
