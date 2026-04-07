d796a2d8a5d5d0aaa4fc74a003819111d42c2e0a
DatsOlena <datsolena@gmail.com>
2026-04-07 12:33:59 +0200

    Fix Ln link

--- src/data/social.js
+++ src/data/social.js
@@ -2,5 +2,5 @@ import { Github, Linkedin } from 'lucide-react'
 
 export const socialLinks = [
   { icon: Github, href: 'https://github.com/DatsOlena', label: 'GitHub' },
-  { icon: Linkedin, href: 'https://www.linkedin.com/in/olena-dats/', label: 'LinkedIn' },
+  { icon: Linkedin, href: 'https://www.linkedin.com/in/olenadats/', label: 'LinkedIn' },
 ]
